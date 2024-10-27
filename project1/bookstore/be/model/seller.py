ea7e0d3e04d9257eedc816e1b5a3cb4ed9b7229f
mtdykly <2224692217@qq.com>
2024-10-27 15:25:16 +0800

    add some changes in seller

--- project1/bookstore/be/model/seller.py
+++ project1/bookstore/be/model/seller.py
@@ -1,21 +1,24 @@
 #import sqlite3 as sqlite
 from be.model import error
 from be.model import db_conn
+from datetime import datetime
 
 
 class Seller(db_conn.DBConn):
     def __init__(self):
-        db_conn.DBConn.__init__(self)
+        super().__init__()
 
     def add_book(
         self,
         user_id: str,
         store_id: str,
         book_id: str,
-        book_json_str: str,
+        book_name: str,  # 从 book_info 改为 book_name
+        book_info: str,  # 新增，用于 books 集合
         stock_level: int,
     ):
         try:
+            # 检查用户、商店是否存在
             if not self.user_id_exist(user_id):
                 return error.error_non_exist_user_id(user_id)
             if not self.store_id_exist(store_id):
@@ -33,12 +36,21 @@ class Seller(db_conn.DBConn):
         #     return 528, "{}".format(str(e))
 
             # 插入新书籍到指定商店的库存中
+                # 插入新书籍到 books 集合
+            self.books_collection.insert_one({
+                "book_id": book_id,
+                "store_id": store_id,
+                "book_info": book_info,
+                "purchase_quantity": 0
+            })
+
+            # 插入新书籍到指定商店的库存中
             result = self.stores_collection.update_one(
                 {"store_id": store_id},
                 {"$push": {
                     "inventory": {
                         "book_id": book_id,
-                        "book_info": book_json_str,
+                        "book_name": book_name,
                         "stock_level": stock_level
                     }
                 }}
@@ -73,10 +85,12 @@ class Seller(db_conn.DBConn):
         #     return 528, "{}".format(str(e))
 
             # 更新书籍的库存数量
-            self.stores_collection.update_one(
+            result = self.stores_collection.update_one(
                 {"store_id": store_id, "inventory.book_id": book_id},
                 {"$inc": {"inventory.$.stock_level": add_stock_level}}
             )
+            if result.modified_count == 0:
+                return error.error_non_exist_store_id(store_id)
 
         except BaseException as e:
             return 530, "{}".format(str(e))
