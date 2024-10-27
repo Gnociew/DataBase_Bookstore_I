8b9b5c1cdc97534938b3b2803933d804a89925b5
Gnociew <138985836+Gnociew@users.noreply.github.com>
2024-10-27 16:33:31 +0800

    resolve some seller-related errors

--- project1/bookstore/be/model/seller.py
+++ project1/bookstore/be/model/seller.py
@@ -54,14 +54,22 @@ class Seller(db_conn.DBConn):
             except Exception as e:
                 return 530, str(e)
 
-            # 插入新书籍到指定商店的库存中
-                # 插入新书籍到 books 集合
-            self.books_collection.insert_one({
-                "book_id": book_id,
-                "store_id": store_id,
-                "book_info": book_info,
-                "purchase_quantity": 0
-            })
+          
+            try:
+                # 检查是否已存在该 book_id
+                existing_book = self.books_collection.find_one({"book_id": book_id})
+                
+                if existing_book is None:
+                    # 如果不存在，则插入新文档
+                    self.books_collection.insert_one({
+                        "book_id": book_id,
+                        "store_id": store_id,
+                        "book_info": book_info,
+                        "purchase_quantity": 0
+                    })
+            except Exception as e:
+                return 530, str(e)
+
 
             # 插入新书籍到指定商店的库存中
             result = self.stores_collection.update_one(
