00f280dd09450779d7ec22711fa99da218e71fa0
mtdykly <2224692217@qq.com>
2024-10-30 21:59:05 +0800

    fix some bugs

--- project1/bookstore/fe/access/auth.py
+++ project1/bookstore/fe/access/auth.py
@@ -13,7 +13,6 @@ class Auth:
         return r.status_code, r.json().get("token")
 
     def register(self, user_id: str, password: str) -> int:
-        print("fe.auth.register")
         json = {"user_id": user_id, "password": password}
         url = urljoin(self.url_prefix, "register")
         r = requests.post(url, json=json)
